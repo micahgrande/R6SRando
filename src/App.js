b07d0761e72ae3eba79b7f946cb05e9dc8ae615e
micah.grande <micah.grande@gmail.com>
2019-08-08 16:42:15 -0700

    added armor/speed option

--- src/App.js
+++ src/App.js
@@ -19,7 +19,14 @@ const SoftBreach = [0, 2, 10, 16]
 const HardBreach = [3, 13]
 const ShieldAttack = [5, 8]
 const ShieldDefend = [20]
-const Camera = [11,13,18]
+const Camera = [11,13,18, 22]
+const Electric = [9, 21]
+const AttackFast = [2, 9, 12, 13, 20]
+const AttackMedium = [0, 1, 3, 4, 6, 8, 10, 11, 14, 15, 16, 17, 18, 19, 21, 23]
+const AttackHeavy = [5, 7, 22]
+const DefendFast = [3, 8, 9]
+const DefendMedium = [0, 1, 2, 6]
+const DefendHeavy = [4, 5, 7]
 
   const AttackStyle = {
     fontSize: "20px",
@@ -57,14 +64,6 @@ const Camera = [11,13,18]
 function R6SRando() {
   
   let [Op, setOp] = useState("? ? ?") //variable shown as the name of who you're playing
-
-  //Randomize all ops. no extra settings
-  const DoAttack = () => {
-    setOp(Attackers[Math.floor(Math.random() * Attackers.length)])
-  }
-  const DoDefend = () => {
-    setOp(Defenders[Math.floor(Math.random() * Defenders.length)])
-  }
 
   //extra settings connected to similarly named buttons
         const SoftBreachToggle = () => {
@@ -120,10 +119,11 @@ function R6SRando() {
             ShieldAttack.forEach(function(element){
               AttackNumber[element]--
             })
-            ShieldAttack.forEach(function(element){
+            ShieldDefend.forEach(function(element){
               DefendNumber[element]--
             })
             console.log("Attack: "+AttackNumber)
+            console.log("Defend: "+ DefendNumber)
             buttontoggle(button)
           }
         }
@@ -133,22 +133,141 @@ function R6SRando() {
             Camera.forEach(function(element){
               DefendNumber[element]++
             })
-            console.log("Attack: " + DefendNumber)
+            console.log("Defend: " + DefendNumber)
             buttontoggle(button)
           }
           else {
             Camera.forEach(function(element){
               DefendNumber[element]--
             })
-            console.log("Attack: " + DefendNumber)
+            console.log("Defend: " + DefendNumber)
+            buttontoggle(button)
+          }
+        }
+        const ElectricToggle = () => {
+          const button = document.getElementById("ElectricButton")
+          if (button.value === "OFF"){
+            Electric.forEach(function(element){
+              DefendNumber[element]++
+            })
+            console.log("Defend: " + DefendNumber)
+            buttontoggle(button)
+          }
+          else {
+            Electric.forEach(function(element){
+              DefendNumber[element]--
+            })
+            console.log("Defend: " + DefendNumber)
+            buttontoggle(button)
+          }
+        }
+        const AttackHeavyToggle = () => {
+          const button = document.getElementById("AttackHeavyButton")
+          if (button.value === "OFF"){
+            AttackHeavy.forEach(function(element){
+              AttackNumber[element]++
+            })
+            console.log("Attack: " + AttackNumber)
+            buttontoggle(button)
+          }
+          else {
+            AttackHeavy.forEach(function(element){
+              AttackNumber[element]--
+            })
+            console.log("Attack: " + AttackNumber)
+            buttontoggle(button)
+          }
+        }
+        const AttackMediumToggle = () => {
+          const button = document.getElementById("AttackMediumButton")
+          if (button.value === "OFF"){
+            AttackMedium.forEach(function(element){
+              AttackNumber[element]++
+            })
+            console.log("Attack: " + AttackNumber)
+            buttontoggle(button)
+          }
+          else {
+            AttackMedium.forEach(function(element){
+              AttackNumber[element]--
+            })
+            console.log("Attack: " + AttackNumber)
+            buttontoggle(button)
+          }
+        }
+        const AttackFastToggle = () => {
+          const button = document.getElementById("AttackFastButton")
+          if (button.value === "OFF"){
+            AttackFast.forEach(function(element){
+              AttackNumber[element]++
+            })
+            console.log("Attack: " + AttackNumber)
+            buttontoggle(button)
+          }
+          else {
+            AttackFast.forEach(function(element){
+              AttackNumber[element]--
+            })
+            console.log("Attack: " + AttackNumber)
+            buttontoggle(button)
+          }
+        }
+        const DefendHeavyToggle = () => {
+          const button = document.getElementById("DefendHeavyButton")
+          if (button.value === "OFF"){
+            DefendHeavy.forEach(function(element){
+              DefendNumber[element]++
+            })
+            console.log("Defend: " + DefendNumber)
+            buttontoggle(button)
+          }
+          else {
+            DefendHeavy.forEach(function(element){
+              DefendNumber[element]--
+            })
+            console.log("Defend: " + DefendNumber)
+            buttontoggle(button)
+          }
+        }
+        const DefendMediumToggle = () => {
+          const button = document.getElementById("DefendHeavyButton")
+          if (button.value === "OFF"){
+            DefendMedium.forEach(function(element){
+              DefendNumber[element]++
+            })
+            console.log("Defend: " + DefendNumber)
+            buttontoggle(button)
+          }
+          else {
+            DefendMedium.forEach(function(element){
+              DefendNumber[element]--
+            })
+            console.log("Defend: " + DefendNumber)
+            buttontoggle(button)
+          }
+        }
+        const DefendFastToggle = () => {
+          const button = document.getElementById("DefendHeavyButton")
+          if (button.value === "OFF"){
+            DefendFast.forEach(function(element){
+              DefendNumber[element]++
+            })
+            console.log("Defend: " + DefendNumber)
+            buttontoggle(button)
+          }
+          else {
+            DefendFast.forEach(function(element){
+              DefendNumber[element]--
+            })
+            console.log("Defend: " + DefendNumber)
             buttontoggle(button)
           }
         }
 
-  const DoExtraAttack = () => {
+  const DoExtraAttack = () => { //called when ATTACK with extra settings button is pressed
     for (var i = 0; i < AttackNumber.length; i++){
       console.log(AttackNumber[i])
-      if(AttackNumber[i] !== 0) {
+      if(AttackNumber[i] > 1) {
         AttackQueue.push(Attackers[i])
       }
     }
@@ -187,32 +306,41 @@ function R6SRando() {
     }
   }
 
-  const testfunction = () => {  //linked to test button
-    console.log(AttackNumber)
-  }
+  // const testfunction = () => {  //linked to test button
+  //   console.log(AttackNumber)
+  // }
 
 
 
   return (
     <div className="App-header">
-        <h1>Click a class to select a random Operator!</h1>
-        <div className = "default">
-          <button style = {AttackStyle} onClick={DoAttack}>ATTACK</button>
-          <button style = {DefendStyle} onClick={DoDefend}>DEFEND</button>
-        </div>
-        <h3>Toggle Operators to include or exclude below!</h3>
-        <div className = "extrasettings">
+        <h1>Toggle Operators to include or exclude below!</h1>
+        <div className = "extrautility">
           <button id='SoftBreachersButton' value = "OFF" style = {ToggleDefault} onClick={SoftBreachToggle}>SOFT BREACHERS</button>
           <button id="HardBreachersButton" value = "OFF" style = {ToggleDefault} onClick={HardBreachToggle}>HARD BREACHERS</button>
           <button id="ShieldButton" value = "OFF" style = {ToggleDefault} onClick={ShieldToggle}>SHIELDS</button>
-          <button id="CameraButton" value = "OFF" style = {ToggleDefault} onClick={CameraToggle}>CAMERAS</button>
+          <button id="CameraButton" value = "OFF" style = {ToggleDefault} onClick={CameraToggle}>EXTRA CAMERAS</button>
+          <button id="ElectricButton" value = "OFF" style = {ToggleDefault} onClick={ElectricToggle}>ELECTRIC</button>
+        </div>
+        <div className = "extraattackarmorspeed">
+          <button id="AttackHeavyButton" value = "OFF" style = {ToggleDefault} onClick={AttackHeavyToggle}>ATTACK HEAVY</button>
+          <button id="AttackMediumButton" value = "OFF" style = {ToggleDefault} onClick={AttackMediumToggle}>ATTACK MEDIUM</button>
+          <button id="AttackFastButton" value = "OFF" style = {ToggleDefault} onClick={AttackFastToggle}>ATTACK FAST</button>
+        </div>
+        <div className = "extrdefendarmorspeed">
+          <button id="DefendHeavyButton" value = "OFF" style = {ToggleDefault} onClick={DefendHeavyToggle}>DEFEND HEAVY</button>
+          <button id="DefendMediumButton" value = "OFF" style = {ToggleDefault} onClick={DefendMediumToggle}>DEFEND MEDIUM</button>
+          <button id="DefendFastButton" value = "OFF" style = {ToggleDefault} onClick={DefendFastToggle}>DEFEND FAST</button>
+        </div>
+        <div>
+          <h1> </h1>
         </div>
         <div className = "doextra">
           <button id="ExtraAttack"style = {AttackStyle} onClick={DoExtraAttack}>ATTACK with extra settings</button>
           <button style = {DefendStyle} onClick={DoExtraDefend}>DEFEND with extra settings</button>
         </div>
         <h2>You are using: {Op}</h2>
-        <button onClick={testfunction}>TEST</button> {/*take out when done*/}
+        {/*<button onClick={testfunction}>TEST</button> {/*take out when done*/}
     </div>
   );
 }
